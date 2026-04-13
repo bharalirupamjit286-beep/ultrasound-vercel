9337797e079002622da5ccfbd374c109ccbbf831
bharalirupamjit286-beep <bharalirupamjit286@gmail.com>
2026-04-13 08:38:49 +0530

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const supabase = createClient(
+  'https://vqncwpgtkellxkncwunk.supabase.co',
+  'sb_publishable_LFiqeWCar0j7ZSPy8qWlGg_BonlarTt'
+);
